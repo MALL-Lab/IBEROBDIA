5af819b5dd102da1d28af35d0e1bac1e3e5309dd
Diego-RNASA <diego@MacBook-Pro-de-Diego-RNASA.local>
2022-10-27 12:28:38 +0200

    update

--- 03_training/sh_generator.r
+++ 03_training/sh_generator.r
@@ -24,7 +24,7 @@ for (i in 1:length(input.paths)) {
       message('Creating directory --outs-- ...')
     }
     # METERLE un -4 para eliminar el ".rds" en los inputh pathts evidatndo el "all_28.rds.sh"
-    sink(paste(exec.dir, substr(input.algs[j], 1, nchar(input.algs[j]) - 7),
+    sink(paste(exec.dir, substr(input.algs[j], 1, nchar(input.algs[j]) - 6),
                substr(input.paths[i], 1, nchar(input.paths[i])-4), '.sh', sep = ''))
     cat("#!/bin/bash \n")
     
@@ -33,10 +33,10 @@ for (i in 1:length(input.paths)) {
     cat(paste("#SBATCH", paste0("--mem=",mem),'\n'))
     cat(paste("#SBATCH", "-N", nodes, '\n'))
     cat(paste("#SBATCH", "-n", ntasks, '\n'))
-    cat(paste("#SBATCH", paste0("--error=", wd, outslurm.dir, "error-", substr(input.algs[j], 1, nchar(input.algs[j]) - 7),
-               substr(input.paths[i], 1, nchar(input.paths[i])-4), ".txt")))
-    cat(paste("#SBATCH", paste0("--output=", wd, outslurm.dir, "output-", substr(input.algs[j], 1, nchar(input.algs[j]) - 7),
-               substr(input.paths[i], 1, nchar(input.paths[i])-4),".txt")))
+    cat(paste("#SBATCH", paste0("--error=", wd, "/", outslurm.dir, "error-", substr(input.algs[j], 1, nchar(input.algs[j]) - 6),
+               substr(input.paths[i], 1, nchar(input.paths[i])-4), ".txt",'\n')))
+    cat(paste("#SBATCH", paste0("--output=", wd, "/", outslurm.dir, "output-", substr(input.algs[j], 1, nchar(input.algs[j]) - 6),
+               substr(input.paths[i], 1, nchar(input.paths[i])-4),".txt",'\n')))
 
     cat("module load cesga/2018 gcc/6.4.0 R/4.0.2", '\n')
     
@@ -46,7 +46,7 @@ for (i in 1:length(input.paths)) {
     
     sink(file = NULL)
     
-    system(paste('sbatch ', '/mnt/netapp2/Store_uni/home/ulc/co/dfe/git/IBEROBDIA/03_training/exec/', substr(input.algs[j], 1, nchar(input.algs[j])-7) ,substr(input.paths[i], 1, nchar(input.paths[i])), '.sh', sep = ''))
+    system(paste('sbatch ', '/mnt/netapp2/Store_uni/home/ulc/co/dfe/git/IBEROBDIA/03_training/exec/', substr(input.algs[j], 1, nchar(input.algs[j])-6) ,substr(input.paths[i], 1, nchar(input.paths[i])-4), '.sh', sep = ''))
     
   } 
 }
