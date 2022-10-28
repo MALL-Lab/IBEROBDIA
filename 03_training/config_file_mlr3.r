e5e1da669a9f2c03a377f417e80ee892d0bd1307
Diego-RNASA <diego@MacBook-Pro-de-Diego-RNASA.local>
2022-10-28 10:52:38 +0200

    update

--- 03_training/config_file_mlr3.r
+++ 03_training/config_file_mlr3.r
@@ -55,7 +55,7 @@ xg.max_depth = c(3, 18)
 # Cross validation parameters
 ## Supra parameters
 cv.inner = 'Holdout'
-cv.outer = 'repeated_cv'
+cv.outer = 'RepCV'
 
 ## Sub parameters
 ## Estos parámetros no los estás pasando a los modelos 
