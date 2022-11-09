65ba9c3730a38b8cced0e8c41449b1baff6393a2
Diego-RNASA <diego@MacBook-Pro-de-Diego-RNASA.local>
2022-11-09 18:43:47 +0100

    update ML

--- 03_training/config_file_mlr3.r
+++ 03_training/config_file_mlr3.r
@@ -2,8 +2,8 @@
 # ====
 setwd('/mnt/netapp2/Store_uni/home/ulc/co/dfe/git/IBEROBDIA/')
 # Data path
-ExperimentName = 'fifth-experiment'
-input.dir.path = '03_training/toRun/fifth-experiment/'
+ExperimentName = 'third-experiment'
+input.dir.path = '03_training/toRun/third-experiment/'
 out.dir.path = '03_training/results/'
 out.path = paste(out.dir.path, ExperimentName, '/', sep = '')
 exec.dir.path = '03_training/exec/'
