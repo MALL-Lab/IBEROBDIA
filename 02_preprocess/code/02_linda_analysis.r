f44ae4253c57f51319baa0a280f9e427a7d46eba
DiegoFE94 <diegolas94@hotmail.com>
2025-01-07 14:36:47 +0100

    prepare data 2025

--- 02_preprocess/code/02_linda_analysis.r
+++ 02_preprocess/code/02_linda_analysis.r
@@ -1,6 +1,19 @@
-# Script to make Linda analysis 
-# -------------------------------------
+# ==============================================================================
+# Script Title: 02_linda_analysis.r
+# Description: This script performs a LINDA analysis
+# ==============================================================================
+# Author/s: @DiegoFE94 (GitHub)
+# Affiliation: Machine Learning for Life Sciences Laboratory (MALL)
+# Email: diego.fedreira@udc.es
+# Date Created: 2024 (Last Update on January 2025)
+# ==============================================================================
+# 0. Set Up
+# ==============================================================================
+# 0.1 Clean Environment
 rm(list = ls())
+set.seed(123)
+
+# 0.2 Load Packages
 library(phyloseq)
 library(MicrobiomeStat)
 library(tibble)
@@ -13,73 +26,60 @@ library(microViz)
 library(ComplexHeatmap)
 library(viridisLite)
 
-set.seed(123)
-make_circos_plot <- function(df, group, col.pal){
-  require(circlize)
-  require(RColorBrewer)
-  require(grDevices)
-  
-  circos.clear()
-  circos.par(start.degree=90)
-  circos.par("canvas.xlim" = c(-2.5, 2.5))  # Ajustar el espacio en blanco horizontal
-  circos.par("canvas.ylim" = c(-2.5, 2.5))
-  chordDiagram(df, grid.col = col.pal,
-               annotationTrack = c('grid'),
-               annotationTrackHeight = c(0.03, 0.01),
-               group = group, big.gap = 10, small.gap = 0.5
-               #col = colorRampPalette(brewer.pal(8, "Set2"))(20)
-  )
-  circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-    xlim = get.cell.meta.data("xlim")
-    ylim = get.cell.meta.data("ylim")
-    sector.name = get.cell.meta.data("sector.index")
-    if(sector.name %in% c(unique(df$up_level),unique(df$down_level))){
-      circos.text(CELL_META$xcenter,
-                  ylim[1] + cm_h(2),
-                  sector.name,
-                  facing = "clockwise",
-                  niceFacing = TRUE,
-                  adj = c(0, 0.5),
-                  cex = 0.7,
-                  #col=grid.col[sector.name],
-                  #font = 2
-      )
-    }
-  }, bg.border = NA)
-}
-# Declare variables 
+# 0.3 Declare or Load Custom Functions
+# source("")
+
+# 0.4 Declare Variables
+# Inputs
+input_path <- "02_preprocess/data/"
+
+# Outputs
+output_path <- "02_preprocess/data/"
+results_path <- "02_preprocess/results/"
+
+# Arguments
 level <- "Species"
 experiment <- "DT2_P_H"
 levels <- c("PreDT2", "Healthy", "DT2")
-model <- '~Status + GS + BMI + MS' # Ms: '~Status + GS + BMI' , DT2: '~Status + GS + BMI + MS'
+model <- "~Status + GS + BMI + MS" # Ms: "~Status + GS + BMI" , DT2: "~Status + GS + BMI + MS"
 id <- "PreDT2vsDT2"
 a <- 0.05
-# Load phyloseq
-phy <- readRDS(file = paste0("02_preprocess/data/phy_",
-                             level,".rds"))
 
-# Change name of the column experiment as Status to perform the analysis
+# ==============================================================================
+# 1. Load and prepare the data
+# ==============================================================================
+# 1.1 Load the phyloseq object
+phy <- readRDS(file = paste0(input_path, "phy_", level, ".rds"))
+
+# 1.2 Change name of the column experiment as Status to perform the analysis
 colnames(phy@sam_data)[colnames(phy@sam_data) == experiment] <- "Status"
-phy@sam_data$Status <-factor(x = as.factor(phy@sam_data$Status),
-                             levels = levels)
+phy@sam_data$Status <- factor(x = as.factor(phy@sam_data$Status),
+                              levels = levels)
 table(phy@sam_data$Status)
 target <- "Status"
 
+# ==============================================================================
+# 2. Perform the LINDA analysis
+# ==============================================================================
+# 2.1 Perform the LINDA method
 linda.res <- linda(phyloseq.obj = phy,
-  feature.dat.type = "count",
-  formula = model,
-  zero.handling = "pseudo-count",
-  alpha = a,
-  p.adj.method = "none",
-  prev.filter = 0, 
-  mean.abund.filter = 0)
-linda_result <- linda.res$output$StatusDT2 # We hace to change this for each status
+                   feature.dat.type = "count",
+                   formula = model,
+                   zero.handling = "pseudo-count",
+                   alpha = a,
+                   p.adj.method = "none",
+                   prev.filter = 0,
+                   mean.abund.filter = 0)
+linda_result <- linda.res$output$StatusDT2 # We hace to change this for each
 
+# 2.2 Convert results into a dataframe
 df2plot <- linda_result %>%
-  dplyr::select(c('log2FoldChange', 'pvalue', 'padj')) %>%
+  dplyr::select(c("log2FoldChange", "pvalue", "padj")) %>%
   rownames_to_column("names")
 
-# Create a volcano  plot
+# ==============================================================================
+# 3. Visualize results in a volcano plot
+# ==============================================================================
 ggplot(df2plot, aes(x = log2FoldChange, y = -log10(pvalue))) +
   geom_point(size = 2, color = ifelse((df2plot$log2FoldChange >= -1 & df2plot$log2FoldChange <= 1) & df2plot$padj > 0.2, "gray",
                                       ifelse(df2plot$pvalue <= 0.05, "red",
@@ -96,63 +96,41 @@ ggplot(df2plot, aes(x = log2FoldChange, y = -log10(pvalue))) +
         axis.title.x = element_text(size = 7),
         axis.text = element_text(size = 7),
         panel.border = element_rect(colour = "black",
-                                    fill=NA,
-                                    size=1))
+                                    fill = NA,
+                                    size = 1))
 
-# select criteri to exclude
+# ==============================================================================
+# 4. Procces the results and save them
+# ==============================================================================
+# 4.1 Select criteria to exclude
 sigtab <- linda_result %>%
-  dplyr::select(c('log2FoldChange', 'pvalue', 'padj','reject')) %>%
+  dplyr::select(c("log2FoldChange", "pvalue", "padj", "reject")) %>%
   mutate(log2FoldChange = abs(log2FoldChange)) %>%
   filter(pvalue < 0.05)
-
 sigtab <- cbind(as(sigtab, "data.frame"),
                 as(tax_table(phy)[rownames(sigtab), ], "matrix"))
 
-# Save phyloseq with only taxones of interest
+# 4.2 Save phyloseq with only taxones of interest
 pruned_pseq <- prune_taxa(x = phy, taxa = rownames(sigtab))
 pruned_pseq@tax_table
 saveRDS(object = pruned_pseq,
-        file = paste0("02_preprocess/data/pruned_", level, "_",id, "_pseq.rds"
-                      ))
-# Save formnated results in csv
-sigtab <- sigtab %>%
-  dplyr::select(c('log2FoldChange', 'pvalue', 'padj',
-                  'reject', 'Family', 'Genus', 'Species'))
-write.csv2(x = sigtab,file = paste0("02_preprocess/results/results_",level,"_",
-                                    id,".csv"),
-           sep = ";", col.names = TRUE,row.names = TRUE)
+        file = paste0(output_path, "pruned_", level, "_", id, "_pseq.rds"))
 
-# Circos plot
-## Prepare data
-if (level == "Genus") {
-  circus <- sigtab %>%
-    dplyr::select(Family, Genus, log2FoldChange) %>%
-    dplyr::rename(up_level = Family, down_level = Genus) %>%
-    arrange(down_level)
-  rownames(circus)<-NULL
-  nm <- unique(c(circus$down_level, circus$up_level))
-  my_colors <- colorRampPalette(brewer.pal(8, "Set2"))(length(nm))
-  grid.col <- data.frame(nm, my_colors)
-  my_palette <- tibble::deframe(grid.col)
-  links <- structure(c(rep('down_level',length(unique(circus$down_level))),
-                       rep('up_level',length(nm)-length(unique(circus$down_level)))),
-                     names = nm)
-} else if (level == "Species") {
-  circus <- sigtab %>% 
-    select(Genus, Species, log2FoldChange) %>%
-    dplyr::rename(up_level = Genus, down_level = Species) %>%
-    arrange(down_level)
-  rownames(circus)<-NULL
-  nm <- unique(c(circus$down_level, circus$up_level))
-  my_colors <- colorRampPalette(brewer.pal(8, "Set2"))(length(nm))
-  grid.col <- data.frame(nm, my_colors)
-  my_palette <- tibble::deframe(grid.col)
-  links <- structure(c(rep('down_level', length(unique(circus$down_level))),
-                       rep('up_level',length(nm)-length(unique(circus$down_level)))),
-                     names = nm)
-} else {
-  stop("Please insert a valid level (Species or Genus)")
-}
-## Print plot
-saveRDS(object = circus, file = "figures/data/PD_Species.rds") # Save name to make a palette
-make_circos_plot(df = circus, group = links , col.pal = my_palette)
+# 4.3 Save formnated results in csv
+sigtab <- sigtab %>%
+  dplyr::select(c("log2FoldChange", "pvalue", "padj",
+                  "reject", "Family", "Genus", "Species"))
+write.csv2(x = sigtab,
+           file = paste0(results_path, "results_", level, "_", id, ".csv"),
+           sep = ";", col.names = TRUE, row.names = TRUE)
+
+# ==============================================================================
+# Session Information
+# ==============================================================================
+sessionInfo()
+
+# Clean up the environment before restarting the session
+rm(list = ls())
+
+# Restart the R session (optional, only if needed)
+# .rs.restartR()
