4b516af137abd039e27ef154d60f5d781515492e
DiegoFE94 <diegolas94@hotmail.com>
2025-01-07 13:53:46 +0100

    prepare data 2025

--- 01_sequencing_data/code/runpipeline.sh
+++ 01_sequencing_data/code/runpipeline.sh
@@ -7,4 +7,5 @@
 #SBATCH --output="/mnt/netapp2/Store_uni/home/ulc/co/dfe/git/IBEROBDIA/01_sequencing_data/out/16S_salida.txt"
 
 module load gcc/6.4.0 R/4.0.2
+
 TMP=/mnt/netapp2/Store_uni/home/ulc/co/dfe/TEMPR Rscript /mnt/netapp2/Store_uni/home/ulc/co/dfe/git/IBEROBDIA/01_sequencing_data/code/01_pipeline_16S.r
